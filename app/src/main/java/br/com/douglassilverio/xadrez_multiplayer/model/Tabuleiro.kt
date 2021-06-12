a17534d293013ac3dbc75d12c88ea8d04c2d57aa
Douglas Ribeiro Silverio <douglas.silverio.ti@gmail.com>
2021-06-12 19:55:33 -0300

    Initial commit

---
+++ app/src/main/java/br/com/douglassilverio/xadrez_multiplayer/model/Tabuleiro.kt
@@ -0,0 +1,12 @@
+package br.com.douglassilverio.xadrez_multiplayer.model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.douglassilverio.xadrez_multiplayer.R
+
+class Tabuleiro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tabuleiro)
+    }
+}
